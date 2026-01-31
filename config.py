198a4caa59e571574d9de058209fbb4a082467b9
Mariana Vangelov <131295956+Marianav330@users.noreply.github.com>
2026-01-31 16:28:32 -0500

    Update shared config.py - central configuration file for all SecureSense modules.
    Added network settings to define ports for sensor, signer, and logger communication, HMAC secret key and timestamp window for security, and database path.

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+# Network
+SENSOR_HOST     = "127.0.0.1"
+SENSOR_PORT     = 5001
+SIGNER_PORT     = 5002
+LOGGER_PORT     = 5003
+DASHBOARD_PORT  = 8080
 
+# Security
+SECRET_KEY       = "Drive_Secure" #Temporary group name - may change
+TIMESTAMP_WINDOW = 10
+
+# Database
+DB_PATH = "secure-driving/logger/events.db" 
